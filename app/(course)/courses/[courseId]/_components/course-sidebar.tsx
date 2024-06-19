87509886b68e4693d100f5e064702a62436ced49
Heber Pereira <isaiahpereira88@gmail.com>
2024-06-19 08:28:13 -0700

    add progress bar component

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -3,8 +3,9 @@ import { auth } from "@clerk/nextjs/server";
 import { Chapter, Course, UserProgress } from "@prisma/client";
 import { redirect } from "next/navigation";
 
-import { db } from "@/lib/db";
 import CourseSidebarItem from "./course-sidebar-item";
+import CourseProgress from "@/components/course-progress";
+import { db } from "@/lib/db";
 
 interface CourseSidebarProps {
   course: Course & {
@@ -31,7 +32,11 @@ const CourseSidebar = async ({ course, progressCount }: CourseSidebarProps) => {
     <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
       <div className="p-8 flex flex-col border-b">
         <h1 className="font-semibold">{course.title}</h1>
-        {/* check purchase and add progress */}
+        {purchase && (
+          <div className="mt-10">
+            <CourseProgress variant="success" value={progressCount} />
+          </div>
+        )}
       </div>
       <div className="flex flex-col w-full">
         {course.chapters.map((chapter) => (
