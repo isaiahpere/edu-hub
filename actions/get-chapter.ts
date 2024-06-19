e8a0d1659b69449319a0fefdf5236e11f912ec5b
Heber Pereira <isaiahpereira88@gmail.com>
2024-06-18 20:30:33 -0700

    add chapter player and get chapter action

---
+++ actions/get-chapter.ts
@@ -0,0 +1,119 @@
+import { db } from "@/lib/db";
+import { Attachment, Chapter } from "@prisma/client";
+
+interface GetChapterProps {
+  userId: string;
+  courseId: string;
+  chapterId: string;
+}
+
+/**
+ *
+ *  To get the course and chapter data. This creates an object if user
+ *  has paid for course.
+ *
+ *  if user did not paid for course, then no data is provided.
+ *
+ * @returns an object of course and chapter data
+ */
+export const getChapter = async ({
+  chapterId,
+  courseId,
+  userId,
+}: GetChapterProps) => {
+  try {
+    const purchase = await db.purchase.findUnique({
+      where: {
+        userId_courseId: {
+          userId,
+          courseId: courseId,
+        },
+      },
+    });
+
+    const course = await db.course.findUnique({
+      where: {
+        id: courseId,
+        isPublished: true,
+      },
+      select: {
+        price: true,
+      },
+    });
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: chapterId,
+        isPublished: true,
+      },
+    });
+
+    if (!chapter || !course) {
+      throw new Error("Chapter or Course not found");
+    }
+
+    // if user paid for course then we will get the data.
+    let muxData = null;
+    let attachments: Attachment[] = [];
+    let nextChapter: Chapter | null = null;
+
+    if (purchase) {
+      attachments = await db.attachment.findMany({
+        where: {
+          courseId,
+        },
+      });
+    }
+
+    if (chapter.isFree || purchase) {
+      muxData = await db.muxData.findUnique({
+        where: {
+          chapterId,
+        },
+      });
+
+      nextChapter = await db.chapter.findFirst({
+        where: {
+          courseId,
+          isPublished: true,
+          position: {
+            gt: chapter?.position,
+          },
+        },
+        orderBy: {
+          position: "asc",
+        },
+      });
+    }
+
+    const userProgress = await db.userProgress.findUnique({
+      where: {
+        userId_chapterId: {
+          userId,
+          chapterId,
+        },
+      },
+    });
+
+    return {
+      course,
+      chapter,
+      muxData,
+      attachments,
+      nextChapter,
+      userProgress,
+      purchase,
+    };
+  } catch (error) {
+    console.log("[GET_CHAPTER]", error);
+    return {
+      chapter: null,
+      course: null,
+      muxData: null,
+      attachments: [],
+      nextChapter: null,
+      userProgress: null,
+      purchase: null,
+    };
+  }
+};
--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,9 +1,65 @@
 import React from "react";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
-const ChapterIdPage = () => {
+import { getChapter } from "@/actions/get-chapter";
+import Banner from "@/components/banner";
+import VideoPlayer from "./_components/video-player";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+  if (!userId) return redirect("/");
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    courseId: params.courseId,
+    chapterId: params.chapterId,
+  });
+
+  if (!chapter || !course) return redirect("/");
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
   return (
     <div>
-      <h1>ChapterIdPage</h1>
+      {userProgress?.isCompleted && (
+        <Banner
+          label="You Already completed this chapter"
+          variant={"success"}
+        />
+      )}
+      {isLocked && (
+        <Banner
+          label="A purchase is required to view chapter"
+          variant={"warning"}
+        />
+      )}
+      <div className="flex flex-col max-w-4xl mx-auto pb-20">
+        <div className="p-4">
+          <VideoPlayer
+            chapterId={params.chapterId}
+            title={chapter.title}
+            courseId={params.courseId}
+            nextChapterId={nextChapter?.id}
+            playbackId={muxData?.playbackId!}
+            isLocked={isLocked}
+            completeOnEnd={completeOnEnd}
+          />
+        </div>
+      </div>
     </div>
   );
 };
